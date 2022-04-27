597153744682ef40f4d56de5292462ec622c68d2
rudrapratapsingh2000 <rc195131@gmail.com>
2022-04-27 23:13:32 +0530

    inbuilt program

---
+++ Febraury/Week-4/Inbuilt01.java
@@ -0,0 +1,16 @@
+
++import java.util.*;
+class Inbuilt01
+{
+public static void main(String args [])
+{4        
+int a=16,b=3;
+System.out.println(Math.pow(a,b));
+System.out.println(Math.sqrt(a));
+System.out.println(Math.ceil(8.2));
+System.out.println(Math.floor(8.2));
+System.out.println(Math.max(a,b));
+System.out.println(Math.min(a,b)); 
+//System.out.println(Math.PI(x));
+}
+}
